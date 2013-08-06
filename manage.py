10e1d0fca8b8862e1c10de7c1b7823db18d124da
yjroot <yjroot@gmail.com>
2013-08-06 14:28:53 +0900

    Removed useless code

--- manage.py
+++ manage.py
@@ -13,13 +13,5 @@ def run():
 def run_public(port=7000):
     app.run(debug=True, use_reloader=True, host='0.0.0.0', port=int(port))
 
-@manager.command
-def initdb():
-    linkle.model.Base.metadata.create_all(linkle.model.engine)
-
-@manager.command
-def dropdb():
-    linkle.model.Base.metadata.drop_all(linkle.model.engine)
-
 if __name__ == '__main__':
     manager.run()
