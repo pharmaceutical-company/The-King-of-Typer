f2b316e09ae18bcf422fb7a6fe5393b1084e78f2
Kang HyoJun <admire9@gmail.com>
2013-09-07 02:05:36 +0900

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#! -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(name='King Of Typer',
+      version='0.0.1',
+      author='pharmaceutical-company',
+      author_email='pharmaceutical-company',
+      install_requires=[
+          'flask==0.10.1', 'flask-script==0.5.3'
+      ])
+
+
